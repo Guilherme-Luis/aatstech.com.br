0cf88c3ad1615be19fb62e6e997117741949a82f
Bruno Borges <63611549+brn-borges@users.noreply.github.com>
2021-03-01 18:56:24 -0300

    Update bd.php

--- bd.php
+++ bd.php
@@ -2,10 +2,6 @@
      $bd_servidor = '127.0.0.1';
      $bd_usuario  = 'root';
      $bd_senha    = '';
-     $bd_banco    = 'bancoAatstech';
-<<<<<<< HEAD
-     
+     $bd_banco    = 'bancoAatstech';   
 ?>
-=======
-?>
->>>>>>> 79d2ff4c98fdff6eadf4e9871fc628b1a38468c9
+
