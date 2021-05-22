aa1decf0c5baa2e9bd15ba7f16d3a354ddaaf61a
Alisson <alisson.junio@hotmail.com>
2021-05-22 11:39:09 -0300

    teste repositorio

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
       <a class="py-2" aria-label="Product">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mx-auto" role="img" viewBox="0 0 24 24"><title>Aatstech Corporation</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
       </a>
-      <a class="py-2 d-none d-md-inline-block" href="#">Home</a>
+      <a class="py-2 d-none d-md-inline-block" href="#">Inicio</a>
       <a class="py-2 d-none d-md-inline-block" href="#produto">Produto e Soluções</a>
       <a class="py-2 d-none d-md-inline-block" href="#precos">Preços</a>
       <a class="py-2 d-none d-md-inline-block" href="#criadores">Criadores</a>
