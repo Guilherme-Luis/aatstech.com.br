1101755e5786295bd14c7a4cc08506d16a3cb24d
Bruno Borges <bruninho-borgesf@hotmail.com>
2021-05-24 20:35:58 -0300

    update 2.5.9 - criação do logo e inserção dela na pagina - Bruno

--- painel/cadastroUsuario.php
+++ painel/cadastroUsuario.php
@@ -20,8 +20,8 @@
     <nav class="container-fluid d-flex flex-column flex-md-row justify-content-between navbar-light bg-light" style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif; 
     font-weight: bold;">
       <a class="py-2 d-none d-md-inline-block" ></a>
-      <a class="py-2" aria-label="Product" href="home.php">Painel do Site</a>
-      <a class="py-2 d-none d-md-inline-block" href="../index.php">Aatstech Corporation ©</a>
+      <a class="py-2" aria-label="Product" href="home.php">Painel</a>
+      <a class="py-2 d-none d-md-inline-block" href="../index.php"><img class="bd-placeholder-img" height="24"  src="../images/logo-c.png" alt=""></a>
       <a class="py-2 d-none d-md-inline-block" onclick="vendasEfetudas()" href="">Vendas Efetudas</a>
       <a class="py-2 d-none d-md-inline-block" href="verDuvidas.php">Dúvidas</a>
       <a class="py-2 d-none d-md-inline-block" onclick="produtos()" href="">Produtos</a> 
@@ -81,8 +81,8 @@
     <footer class="container py-5 margin-top-0 margin-right-auto margin-bottom-0 margin-left-auto">
     <div class="row">
       <div class="col-12 col-md-2">
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="d-block mb-2" role="img" viewBox="0 0 24 24"><title>Produto</title><circle cx="12" cy="12" r="10"></circle><path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94"></path></svg>
-        <small class="d-block mb-3 text-muted"><b>Aatstech</br>Corporation </br> © 2021</b></small>
+      <img class="bd-placeholder-img" height="24"  src="../images/logo-c.png" alt="">
+        <small class="d-block mb-3 text-muted"><b>© 2021</b></small>
       </div>
       <div class="col-md-3">
         <h5>Conteúdo</h5>
